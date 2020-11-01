4e552367ab2db251004abaf88a9849f4d8364c40
Jinwoong <okokrt@gmail.com>
2020-11-01 19:49:21 +0900

    Update home_screen.dart include nav

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hotel_management/screens/room_detail_screen.dart';
+import 'package:hotel_management/screens/room_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,8 +8,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _children = [
+    RoomScreen(),
+    RoomDetailScreen(),
+    RoomScreen(),
+  ];
+  void _onTap(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: _children[_currentIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onTap,
+        currentIndex: _currentIndex,
+        items: [
+          // ignore: deprecated_member_use
+          new BottomNavigationBarItem(
+            title: Padding(padding: EdgeInsets.all(0)),
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person_outline),
+          ),
+          // ignore: deprecated_member_use
+          new BottomNavigationBarItem(
+            title: Padding(padding: EdgeInsets.all(0)),
+            icon: Icon(Icons.king_bed_outlined),
+            activeIcon: Icon(Icons.king_bed_outlined),
+          ),
+          // ignore: deprecated_member_use
+          new BottomNavigationBarItem(
+            title: Padding(padding: EdgeInsets.all(0)),
+            icon: Icon(Icons.car_rental),
+            activeIcon: Icon(Icons.car_rental),
+          )
+        ],
+      ),
+    );
   }
 }
