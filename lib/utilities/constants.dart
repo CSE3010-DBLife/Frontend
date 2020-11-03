3c2ff54bc5250aaa52e36190d4fc4ff7b9202fce
Jinwoong <okokrt@gmail.com>
2020-11-03 22:28:40 +0900

    Update constants.dart include subtitle1, status colors, box decorations

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData darkThemeData = ThemeData(
   scaffoldBackgroundColor: Color(0xFF121212),
   textTheme: TextTheme(
+    subtitle1: TextStyle(color: Color(0xFFE7E7E7), fontSize: 15, fontFamily: 'Godo-M'),
     subtitle2: TextStyle(color: Color(0xFFD1D1D1), fontSize: 10, fontFamily: 'Godo-M'),
     headline6: TextStyle(color: Color(0xFFE7E7E7), fontSize: 20, fontFamily: 'Godo-M'),
   ),
@@ -11,4 +12,24 @@ ThemeData darkThemeData = ThemeData(
       backgroundColor: Color(0xFF181818),
       selectedIconTheme: IconThemeData(color: Colors.white),
       unselectedIconTheme: IconThemeData(color: Color(0xFFD1D1D1))),
+);
+
+Color statusRed = Color(0xFFE65F3F);
+Color statusGreen = Color(0xFF04CF5B);
+Color statusOrange = Color(0xFFFDBC00);
+Color statusGrey = Color(0xFFD1D1D1);
+
+BoxDecoration cardDecoration = BoxDecoration(
+  color: Color(0xFF202020),
+  borderRadius: BorderRadius.circular(8.0),
+);
+
+BoxDecoration cardInnerDecoration = BoxDecoration(
+  color: Color(0xFF141414),
+  borderRadius: BorderRadius.circular(18.0),
+);
+
+BoxDecoration buttonDecoration = BoxDecoration(
+  color: Color(0xFF202020),
+  borderRadius: BorderRadius.circular(8.0),
 );
