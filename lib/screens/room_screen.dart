99165a6b7228c345ef319c068caec6626dd50a32
Jinwoong <okokrt@gmail.com>
2020-11-17 19:59:42 +0900

    Remove unused import line in room screen

--- lib/screens/room_screen.dart
+++ lib/screens/room_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_management/components/custom_title.dart';
-import 'package:hotel_management/components/reusable_card.dart';
-import 'package:hotel_management/components/reusable_status.dart';
 import 'package:hotel_management/futurebuilders/room/summary_card_builder.dart';
-import 'package:hotel_management/screens/room_detail_screen.dart';
-import 'package:hotel_management/utilities/constants.dart';
 
 class RoomScreen extends StatefulWidget {
   @override
