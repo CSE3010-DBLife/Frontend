e8e4b3729b988e5b627446f35a0b4790efe431d6
Jinwoong <okokrt@gmail.com>
2020-11-17 19:49:02 +0900

    Update room_screen include room builder

--- lib/screens/room_screen.dart
+++ lib/screens/room_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hotel_management/components/custom_title.dart';
 import 'package:hotel_management/components/reusable_card.dart';
 import 'package:hotel_management/components/reusable_status.dart';
+import 'package:hotel_management/futurebuilders/room/summary_card_builder.dart';
 import 'package:hotel_management/screens/room_detail_screen.dart';
 import 'package:hotel_management/utilities/constants.dart';
 
@@ -24,99 +25,9 @@ class _RoomScreenState extends State<RoomScreen> {
             ),
             Row(
               children: [
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) {
-                        return RoomDetailScreen();
-                      }));
-                    },
-                    child: ReusableCard(
-                      decoration: cardDecoration,
-                      cardChild: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                decoration: cardInnerDecoration,
-                                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                                child: Text(
-                                  '방 101호',
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(right: 8.0),
-                                child: ReusableStatus(color: statusRed),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 8.0, 16.0, 8.0),
-                                child: Text(
-                                  '사용중',
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                SummaryCardBuilder(),
                 SizedBox(width: 16),
-                Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) {
-                        return RoomDetailScreen();
-                      }));
-                    },
-                    child: ReusableCard(
-                      decoration: cardDecoration,
-                      cardChild: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                decoration: cardInnerDecoration,
-                                padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                                child: Text(
-                                  '방 102호',
-                                  style: Theme.of(context).textTheme.subtitle2,
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(right: 8.0),
-                                child: ReusableStatus(color: statusGreen),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 8.0, 16.0, 8.0),
-                                child: Text(
-                                  '예약됨',
-                                  style: Theme.of(context).textTheme.headline6,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+                SummaryCardBuilder(),
               ],
             ),
           ],
