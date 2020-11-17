890a88ba16a22f9e168d77a193513b55955fcdee
Jinwoong <okokrt@gmail.com>
2020-11-17 19:49:50 +0900

    Update room_detail_screen include room_detail builder

--- lib/screens/room_detail_screen.dart
+++ lib/screens/room_detail_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hotel_management/components/custom_title.dart';
-import 'package:hotel_management/components/reusable_card.dart';
 import 'package:hotel_management/components/reusable_status.dart';
+import 'package:hotel_management/futurebuilders/room_detail/contact_card_builder.dart';
+import 'package:hotel_management/futurebuilders/room_detail/member_card_builder.dart';
 import 'package:hotel_management/screens/request_screen.dart';
 import 'package:hotel_management/utilities/constants.dart';
 
@@ -32,184 +33,10 @@ class _RoomDetailScreenState extends State<RoomDetailScreen> {
                 ],
               ),
             ),
-            ReusableCard(
-              decoration: cardDecoration,
-              cardChild: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        decoration: cardInnerDecoration,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(
-                          '예약대표 연락처',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      SizedBox(width: 8),
-                      Text(
-                        '주차 번호 : 07 누 7777',
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      SizedBox(width: 8),
-                      Text(
-                        '경기도 안산시 상록구 사3동 한양대학로 55',
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        decoration: cardInnerDecoration,
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          '010 - 7777 - 7777',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(right: 8.0),
-                        child: Text(
-                          '유진웅',
-                          style: Theme.of(context).textTheme.subtitle1,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            ContactCardBuilder(),
             SizedBox(height: 16),
-            ReusableCard(
-              decoration: cardDecoration,
-              cardChild: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        decoration: cardInnerDecoration,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(
-                          '구성원',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Container(
-                          decoration: cardInnerDecoration,
-                          padding: EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Text(
-                                '성인',
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    '2명',
-                                    style: Theme.of(context).textTheme.subtitle1,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 8),
-                      Expanded(
-                        child: Container(
-                          decoration: cardInnerDecoration,
-                          padding: EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              Text(
-                                '미성년자',
-                                style: Theme.of(context).textTheme.subtitle1,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  Text(
-                                    '2명',
-                                    style: Theme.of(context).textTheme.subtitle1,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+            MemberCardBuilder(),
             SizedBox(height: 16.0),
-            ReusableCard(
-              decoration: cardDecoration,
-              cardChild: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        decoration: cardInnerDecoration,
-                        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                        child: Text(
-                          '세부사항',
-                          style: Theme.of(context).textTheme.subtitle2,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Container(
-                                  margin: EdgeInsets.only(right: 4.0),
-                                  child: Text(
-                                    '18형/트윈 베드/800,000원',
-                                    style: Theme.of(context).textTheme.subtitle1,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
           ],
         ),
       ),
