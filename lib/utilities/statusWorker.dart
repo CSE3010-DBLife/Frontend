126f52b54af2c746bfed4b84be3d8feb79351fb3
Jinwoong <okokrt@gmail.com>
2020-11-24 20:38:48 +0900

    Create statusWorker.dart for filiter

---
+++ lib/utilities/statusWorker.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:hotel_management/utilities/constants.dart';
+
+class StatusWorker {
+  static String getRoomStatus(String status) {
+    if (status == "0")
+      return "예약 가능";
+    else if (status == "1")
+      return "예약 됨";
+    else if (status == "2")
+      return "사용 중";
+    else if (status == "3")
+      return "외출 중";
+    else
+      return "오류";
+  }
+
+  static Color getRoomStatusColor(String status) {
+    if (status == "0")
+      return statusGrey;
+    else if (status == "1")
+      return statusGreen;
+    else if (status == "2")
+      return statusRed;
+    else if (status == "3")
+      return statusOrange;
+    else
+      return Colors.black;
+  }
+}
